9d6cd3e4f5a43a91d02b921fd8f469dbf368bed2
AndroidOS <burrowsgit@gmail.com>
2020-04-06 08:14:13 +1000

    Added ListAdapter

---
+++ app/src/main/java/com/manuelcarvalho/portfolioapp/view/ListAdapter.kt
@@ -0,0 +1,46 @@
+package com.manuelcarvalho.portfolioapp.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.manuelcarvalho.portfolioapp.R
+import kotlinx.android.synthetic.main.list_view.view.*
+
+
+class ListAdapter(val magList: ArrayList<Double>) :
+    RecyclerView.Adapter<ListAdapter.MagViewHolder>() {
+
+    fun updatelist(newGitJobsList: List<Double>) {
+        magList.clear()
+        magList.addAll(newGitJobsList)
+        notifyDataSetChanged()
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MagViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view: View = inflater.inflate(
+            R.layout.list_view
+            , parent, false
+        )
+        return MagViewHolder(view)
+    }
+
+    override fun getItemCount() = magList.size
+
+    override fun onBindViewHolder(holder: MagViewHolder, position: Int) {
+        holder.view.textView.text = magList[position].toString()
+
+
+
+        holder.view.setOnClickListener {
+            Navigation.findNavController(it)
+                .navigate(FirstFragmentDirections.actionFirstFragmentToSecondFragment())
+        }
+
+    }
+
+    class MagViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+}

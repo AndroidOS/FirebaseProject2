0a1294cb800b451bcc4e73c3053f4e91138547ab
AndroidOS <burrowsgit@gmail.com>
2020-04-11 16:11:54 +1000

    Added data to viewmodel

--- app/src/main/java/com/manuelcarvalho/portfolioapp/viewmodel/AppViewModel.kt
+++ app/src/main/java/com/manuelcarvalho/portfolioapp/viewmodel/AppViewModel.kt
@@ -3,9 +3,11 @@ package com.manuelcarvalho.portfolioapp.viewmodel
 import android.app.Application
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.manuelcarvalho.portfolioapp.model.Part
+import kotlinx.coroutines.launch
 
 
 private const val TAG = "AppViewModel"
@@ -20,9 +22,10 @@ class AppViewModel(application: Application) : BaseViewModel(application) {
     fun refresh() {
         Log.w(TAG, "Refresh ")
         db.collection("carts")
-            .whereEqualTo("computer", "VIC-20")
+            .whereEqualTo("manufacturer", "Mach. Lang. ")
             .get()
             .addOnSuccessListener { documents ->
+                createList(documents)
                 for (document in documents) {
                     Log.d(TAG, "${document.id} => ${document.data}")
                 }
@@ -33,5 +36,31 @@ class AppViewModel(application: Application) : BaseViewModel(application) {
 
     }
 
+
+    private fun createList(documents: QuerySnapshot) {
+        launch {
+            var cartList = mutableListOf<Part>()
+            //Log.d(TAG, "${documents}")
+            for (document in documents) {
+                Log.d(TAG, "${document}")
+                cartList.add(
+                    Part(
+                        document.data["manufacturer"].toString(),
+                        document.data["computer"].toString(),
+                        document.data["catridge"].toString(),
+                        document.data["condition"].toString(),
+                        document.data["partNum"].toString(),
+                        document.data["romUse"].toString(),
+                        document.data["gamePlay"].toString(),
+                        document.data["rarity"].toString(),
+                        document.data["yearMade"].toString()
+                    )
+                )
+            }
+            carts.value = cartList
+        }
+        //Log.d(TAG, "${cartList.get(0)}")
+
+    }
 
 }

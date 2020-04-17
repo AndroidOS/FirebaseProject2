9d5deb7b760d2dbf61966ec2314f3e65cd23ec63
AndroidOS <burrowsgit@gmail.com>
2020-04-17 21:01:39 +1000

    Added Buuton listener in AddFragment

--- app/src/main/java/com/manuelcarvalho/portfolioapp/view/AddFragment.kt
+++ app/src/main/java/com/manuelcarvalho/portfolioapp/view/AddFragment.kt
@@ -1,12 +1,16 @@
 package com.manuelcarvalho.portfolioapp.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.manuelcarvalho.portfolioapp.R
+import com.manuelcarvalho.portfolioapp.model.Part
+import kotlinx.android.synthetic.main.fragment_add.*
 
+private const val TAG = "AddFragment"
 class AddFragment : Fragment() {
 
     override fun onCreateView(
@@ -15,6 +19,32 @@ class AddFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_add.setOnClickListener {
+
+            val part = Part(
+                ed_manufacturer.text.toString(),
+                "Commodore VIC-20",
+                ed_cartridge.text.toString(),
+                ed_condition.text.toString(),
+                ed_partNum.text.toString(),
+                ed_romUse.text.toString(),
+                ed_gamePlay.text.toString(),
+                ed_rarity.text.toString(),
+                ed_yearMade.text.toString()
+            )
+
+            Log.d(TAG, "$part")
+
+        }
+    }
+
+    private fun checkForEmptyString(str: String): Boolean {
+        return str == ""
     }
 
 }
